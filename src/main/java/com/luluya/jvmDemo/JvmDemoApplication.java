83db6b40f42d205bdc2262c2c34117b2abb6d328
chenjialu <chenjialu0804@163.com>
2019-05-20 09:07:59 +0800

    init

---
+++ src/main/java/com/luluya/jvmDemo/JvmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luluya.jvmDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmDemoApplication.class, args);
+	}
+
+}
